d39d8928bacd5c521e489328df2e5272bdf38a17
Ryan Lee <ryan.leegr@gmail.com>
2022-04-21 14:50:02 -0400

    first commit

---
+++ Diceeswiftui/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Diceeswiftui
+//
+//  Created by ryan lee on 4/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+   @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                
+                Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        
+                                        
+                }
+                .background(Color.red)
+                Spacer()
+            }
+        }
+    }
+}
+
+
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Diceeswiftui/DiceeswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeswiftuiApp.swift
+//  Diceeswiftui
+//
+//  Created by ryan lee on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
